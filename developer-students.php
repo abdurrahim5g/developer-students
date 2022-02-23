b9dcc9d8cfce69c43ccf72670f3babdac49f5965
f-rahim <freelancerrahim26@gmail.com>
2022-02-23 21:58:47 +0600

    Developer Students initial commit

---
+++ developer-students.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:     Developer Student
+ * Plugin URI:      https://github.com/f-rahim/developer-students
+ * Description:     Simple suudent menagement plugin.
+ * Author:          Abdur Rahim
+ * Author URI:      https://fackbook.com/devrahim26
+ */
